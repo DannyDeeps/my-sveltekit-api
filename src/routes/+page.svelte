e073096419f69d836a5f435929cbeccde97e3576
Daniel Huggins <contactdanielhuggins@gmail.com>
2023-11-04 08:36:22 +0000

    work on CRUD api for json-server

--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<h1>Available Resources</h1>
-
-{#each [{name: 'forms', url: '/forms'}] as res}
-    <a href={res.url}>{res.name}</a>
-{/each}
---
+++ src/routes/create/[...path]/+server.js
@@ -0,0 +1,28 @@
+import { json } from '@sveltejs/kit';
+import { env } from "$env/dynamic/private";
+
+/** @type {import("./$types").RequestHandler} */
+export async function GET({ params }) {
+  let newForm = {
+    name: "newform",
+    steps: []
+  };
+
+  console.log(`${env.DB_HOST}/${params.path}`);
+
+  const response = await fetch(`${env.DB_HOST}/${params.path}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newForm)
+  });
+
+  let data = {};
+
+  if (response.ok) {
+    data = await response.json();
+  }
+
+  return json(data);
+}
