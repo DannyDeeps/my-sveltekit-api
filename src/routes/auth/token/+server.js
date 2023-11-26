b1aa0c6d996a54af3525e5d2de609be01275901c
Daniel Huggins <contactdanielhuggins@gmail.com>
2023-11-26 12:44:56 +0000

    general early development

--- src/routes/auth/token/+server.js
+++ src/routes/auth/token/+server.js
@@ -3,25 +3,25 @@ import { env } from '$env/dynamic/private';
 import { randomUUID } from 'crypto';
 
 /** @type {import('./$types').RequestHandler} */
-export async function POST({cookies, request}) {\
+export async function POST({cookies, request}) {
+
 	const data = await request.json();
 
 	const response = await fetch(`${env.DB_HOST}/accounts/${data.account}`);
-	if (!response.ok ) {
+	if (!response.ok) {
 		return json({ message: 'Authorization Failed' });
 	}
-	
-	const token = randomUUID();
-	const expire = new Date('+1 day');
-	
-	const storeToken = await fetch(`${env.DB_HOST}/tokens/${data.account}`, {
-		method: 'PUT',
-		body: JSON.stringify({ token })
+
+  const token = {
+    account: data.account,
+    uuid: randomUUID(),
+    expire: new Date()
+  };
+
+	const storeToken = await fetch(`${env.DB_HOST}/tokens`, {
+		method: 'POST',
+		body: JSON.stringify(token)
 	});
 
-	if (!storeToken.ok) {
-		return json({ message: 'Internal Error' });
-	}
-	
-	return json({token, expire});
+  return storeToken;
 }
